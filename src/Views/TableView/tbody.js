821e940765617d10f27ade7a0610dad79e0f33f7
Kristian <mkristian@live.ru>
2022-05-23 01:41:37 +0400

    Added react transition

--- src/Views/TableView/tbody.js
+++ src/Views/TableView/tbody.js
@@ -1,65 +1,81 @@
-import style from './styles/index.module.scss';
-import Button from '../Button'
+import cn from 'classnames';
+import { useState } from 'react';
+import { CSSTransition, Transition, TransitionGroup } from 'react-transition-group';
 import { CAR_FIELDS } from '../../const';
+import Button from '../Button';
 import EditableField from './EditableField';
+import style from './styles/index.module.scss';
 
 const Tbody = ({ cars, deleteItem, isEditItem, editItem, onEditItem, editItemValue, saveChanges, getSelectedItems }) => {
+    const [status, setStatus] = useState(false)
+
     return (
         <tbody className={style.tbody}>
-            {cars?.map(({ id, brand, model, maxSpeed, speedType }) => {
+            <TransitionGroup className='todo'>
+                {cars?.map(({ id, brand, model, maxSpeed, speedType }) => (
+                    <CSSTransition
+                        key={id}
+                        timeout={200}
+                        classNames={{
+                            enterActive: style['item-enter'],
+                            enterDone: style['item-enter-active'],
+                            exitActive: style['item-exit'],
+                            exitDone: style['item-exit-active'],
+                        }}
+                    >
+                        <tr className={cn(style.tbody__tr, style[`tbody__tr_`])} key={id}>
+                            <td className={style.tbody__td}>
+                                <input data-id={id} onChange={getSelectedItems} type="checkbox" />
+                            </td>
+                            <td className={style.tbody__td}>
+                                <EditableField
+                                    id={id}
+                                    editItemId={editItem?.id}
+                                    editItemField={editItem?.field}
+                                    isEditItem={isEditItem}
+                                    saveChanges={saveChanges}
+                                    onEditItem={onEditItem}
+                                    field={CAR_FIELDS.brand}
+                                    editItemValue={editItemValue}
+                                    value={brand}
+                                />
+                            </td>
+                            <td className={style.tbody__td}>
+                                <EditableField
+                                    id={id}
+                                    editItemId={editItem?.id}
+                                    editItemField={editItem?.field}
+                                    isEditItem={isEditItem}
+                                    saveChanges={saveChanges}
+                                    onEditItem={onEditItem}
+                                    field={CAR_FIELDS.model}
+                                    editItemValue={editItemValue}
+                                    value={model}
+                                />
+                            </td>
+                            <td className={style.tbody__td}>
+                                <EditableField
+                                    id={id}
+                                    editItemId={editItem?.id}
+                                    editItemField={editItem?.field}
+                                    isEditItem={isEditItem}
+                                    saveChanges={saveChanges}
+                                    onEditItem={onEditItem}
+                                    field={CAR_FIELDS.maxSpeed}
+                                    editItemValue={editItemValue}
+                                    value={maxSpeed}
+                                    type='number'
+                                />
+                                {speedType}/h
+                            </td>
+                            <td className={style.tbody__td}>
+                                <Button onClick={() => deleteItem(id)} theme='danger'>DELETE</Button>
+                            </td>
+                        </tr>
+                    </CSSTransition>
 
-                return (
-                    <tr className={style.tbody__tr} key={id}>
-                        <td className={style.tbody__td}>
-                            <input data-id={id} onChange={getSelectedItems} type="checkbox" />
-                        </td>
-                        <td className={style.tbody__td}>
-                            <EditableField
-                                id={id}
-                                editItemId={editItem?.id}
-                                editItemField={editItem?.field}
-                                isEditItem={isEditItem}
-                                saveChanges={saveChanges}
-                                onEditItem={onEditItem}
-                                field={CAR_FIELDS.brand}
-                                editItemValue={editItemValue}
-                                value={brand}
-                            />
-                        </td>
-                        <td className={style.tbody__td}>
-                            <EditableField
-                                id={id}
-                                editItemId={editItem?.id}
-                                editItemField={editItem?.field}
-                                isEditItem={isEditItem}
-                                saveChanges={saveChanges}
-                                onEditItem={onEditItem}
-                                field={CAR_FIELDS.model}
-                                editItemValue={editItemValue}
-                                value={model}
-                            />
-                        </td>
-                        <td className={style.tbody__td}>
-                            <EditableField
-                                id={id}
-                                editItemId={editItem?.id}
-                                editItemField={editItem?.field}
-                                isEditItem={isEditItem}
-                                saveChanges={saveChanges}
-                                onEditItem={onEditItem}
-                                field={CAR_FIELDS.maxSpeed}
-                                editItemValue={editItemValue}
-                                value={maxSpeed}
-                                type='number'
-                            />
-                            {speedType}/h
-                        </td>
-                        <td className={style.tbody__td}>
-                            <Button onClick={() => deleteItem(id)} theme='danger'>DELETE</Button>
-                        </td>
-                    </tr>
-                )
-            })}
+                ))}
+            </TransitionGroup>
         </tbody>
     )
 }
